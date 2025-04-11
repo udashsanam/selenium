6fec057e007f5676e740e3893c9e26c63eeb4c9b
udashsanam <udashsanam@gmail.com>
2025-04-11 17:55:41 +0545

    add: java script executor

--- src/test/java/org/learn/WindowSizeTest.java
+++ src/test/java/org/learn/WindowSizeTest.java
@@ -1,20 +1,62 @@
 package org.learn;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 public class WindowSizeTest {
 
-    private WebDriver webDriver;
+    private WebDriver driver;
     @BeforeEach
     public void setUp(){
-        webDriver = new ChromeDriver();
+        driver = new ChromeDriver();
     }
 
     @AfterEach
     public void closeUp(){
-        webDriver.quit();
+        driver.quit();
+    }
+
+    @Test
+    public void test() throws InterruptedException, IOException {
+        driver.get("https://google.com");
+
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+
+        System.out.println(width);
+        System.out.println(height);
+        Dimension size = driver.manage().window().getSize();
+        Assertions.assertEquals(size.width, width);
+        Assertions.assertEquals(size.height, height);
+        driver.manage().window().setSize(new Dimension(width + 200, height + 400));
+
+        Point point = driver.manage().window().getPosition();
+        System.out.println(point.x);
+        System.out.println(point.y);
+        driver.manage().window().setPosition(new Point(point.x + 10,point.y + 10));
+
+       File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("./image"+ Math.random()+".png"));
+
+        Thread.sleep(2000);
+
+    }
+
+    @Test
+    public void javaScriptExecutor() throws InterruptedException {
+        driver.get("https://google.com");
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        WebElement button = driver.findElement(By.cssSelector(".gNO89b"));
+        WebElement search = driver.findElement(By.cssSelector(".gLFyf"));
+        javascriptExecutor.executeScript("arguments[0].value = 'Aashish'; arguments[1].click(); ",search, button);
+        Thread.sleep(5000);
+
+
     }
 }
